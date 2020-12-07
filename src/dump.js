08022d79287462c7af66e39d55d983eef748091f
Namratha <namrathapmd@gmail.com>
2020-12-07 12:28:03 -0600

    added html and css for the experience/education section

---
+++ src/dump.js
@@ -0,0 +1,42 @@
+<div class="container text-center">
+    <h2>Education</h2>
+    <div class="row">
+        <div class="col-md-5 text-left">
+            <h5>Fullstack Academy of Code</h5>
+            <p>New York, NY</p>
+            <p>Certificate in Full Stack Web Development, Software Engineering</p>
+        </div>
+        <div class="col-md-2">
+            <img src="../assets/fs-logo.png" class="img-fluid exp-logo" alt="Responsive image">
+                    </div>
+            <div class="col-md-5">
+                <h6>August 2020- October 2020</h6>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5 text-left">
+                <h5>Rochester Institute of Technology</h5>
+                <p>Rochester, NY</p>
+                <p>Master of Science, Electrical Engineering</p>
+            </div>
+            <div class="col-md-2">
+                <img src="../assets/rit-logo.png" class="img-fluid exp-logo" alt="Responsive image">
+                    </div>
+                <div class="col-md-5">
+                    <h6>August 2014- May 2017</h6>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5 text-left">
+                    <h5>Sri Jayachamarajendra College of Engineering</h5>
+                    <p>Mysore, Karnataka, India</p>
+                    <p>Bachelor of Engineering, Electronics & Communication Engineering</p>
+                </div>
+                <div class="col-md-2">
+                    <img src="../assets/sjce-logo.png" class="img-fluid exp-logo" alt="Responsive image">
+                    </div>
+                    <div class="col-md-5">
+                        <h6>August 2010- May 2014</h6>
+                    </div>
+                </div>
+            </div>
